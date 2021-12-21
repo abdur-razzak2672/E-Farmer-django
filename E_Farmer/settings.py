020873a06ec38863f93c66473f8b342cdd9605be
abdur-razzak2672 <abdur35-2672@diu.edu.bd>
2021-12-22 01:33:29 +0600

    bug fixing

--- E_Farmer/settings.py
+++ E_Farmer/settings.py
@@ -1,9 +1,5 @@
 from pathlib import Path
 import os
-<<<<<<< HEAD
-=======
-
->>>>>>> 63c2412343007fd9937801ab9787add02e8c4f74
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -103,11 +99,6 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-<<<<<<< HEAD
-=======
-
->>>>>>> 63c2412343007fd9937801ab9787add02e8c4f74
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
@@ -115,8 +106,3 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-
-<<<<<<< HEAD
- 
-=======
->>>>>>> 63c2412343007fd9937801ab9787add02e8c4f74
