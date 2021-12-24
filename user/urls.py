90c07e1dee0ca7a31e8412abc0feefcca4e7d162
abdur-razzak2672 <abdur35-2672@diu.edu.bd>
2021-12-25 02:04:41 +0600

    user registrations system bug fix

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('user_registration', views.register, name='register'),
     path('login_user',views.login_user, name = "login"),
     path('logout', views.logoutuser, name='logoutuser'),
  
--- user/views.py
+++ user/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .decorators import *
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect ('login')
+        
+    else:
+        form = UserCreationForm()
+            
+    context = {'form':form}
+    return render(request, 'authenticate/register.html', context)
+
 
 def login_user(request):
     if request.method =="POST":
